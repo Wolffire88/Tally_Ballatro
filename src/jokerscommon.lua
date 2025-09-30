ad48653c55fb7a4d66cb7bdb1cf591cf21ad5bd7
Wolffire25 <robbie.bana.13@gmail.com>
2025-09-30 02:40:13 -0400

    Added check for aces and twos in not a trampoline

--- src/jokerscommon.lua
+++ src/jokerscommon.lua
@@ -68,7 +68,10 @@ SMODS.Atlas({
     py = 95
 })
 
-
+--[[
+Joe Hawley Joe Hawley (Common)
+Joker retriggers all cards played in the previous round
+]]--
 SMODS.Joker{
     key = "joehawleyjoehawley",
     config = { extra = { repetitions = 1 } },
@@ -97,6 +100,11 @@ SMODS.Joker{
     end
 }
 
+--[[
+Purple Tie (Common)
+Has a one in six chance to generate a purple seal on a played card
+One in 4 chance for a purple seal to give an extra tarot
+]]
 SMODS.Joker{
     key = "purpletie",
     config = { extra = { seal_chance = 6, tarot_chance = 4}},
@@ -126,6 +134,8 @@ SMODS.Joker{
         if context.before then
             if pseudorandom('tieseal') < G.GAME.probabilities.normal / card.ability.extra.seal_chance then
                 local rand_card = pseudorandom_element(G.play.cards, 'tieseal_card')
+
+                --Generate the actual purple seal
                 G.E_MANAGER:add_event(Event({
                     trigger = 'after',
                     delay = '1.3',
@@ -143,11 +153,12 @@ SMODS.Joker{
         end
 
         -- extra tarot chance
-        if context.discard then
+        if context.discard and context.other_card:get_seal() == 'Purple' and not context.other_card.debuff then
             if pseudorandom('tietarot') < G.GAME.probabilities.normal / card.ability.extra.tarot_chance and
-                #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit and
-                context.other_card:get_seal() == 'Purple' and not context.other_card.debuff then
+                #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
                 G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
+
+                --Create the tarot
                 G.E_MANAGER:add_event(Event({
                     trigger = 'after',
                     delay = '1.3',
@@ -160,6 +171,7 @@ SMODS.Joker{
                     end
                 }))
 
+                --Return an extra tarot message
                 return {
                     message = localize('k_plus_tarot'),
                     colour = G.C.PURPLE,
@@ -170,6 +182,11 @@ SMODS.Joker{
     end
 }
 
+--[[
+English Chap (Common)
+Retriggers the first played card of a given rank 3 additional times.
+The rank changes every round.
+]]
 SMODS.Joker{
     key = "englishchap",
     config = { extra = { retriggers = 3 }},
@@ -195,6 +212,7 @@ SMODS.Joker{
     calculate = function(self, card, context)
         if card.debuff then return nil end
         if context.repetition and context.cardarea == G.play then
+            --Check for retriggerable cards
             local is_retrigger_card = false
             for i = 1, #context.scoring_hand do
                 if context.scoring_hand[i]:get_id() == G.GAME.current_round.english_chap_rank.id and
@@ -213,6 +231,10 @@ SMODS.Joker{
     end
 }
 
+--[[
+Spaghetti Bathtub (Common)
+Gives xchips. Loses xchips per card played
+]]
 SMODS.Joker{
     key = "spaghettibathtub",
     config = { extra = { xchips = 2, xchip_loss = 0.01, eaten = false }},
@@ -239,6 +261,7 @@ SMODS.Joker{
 
         if not context.blueprint and context.individual and context.cardarea == G.play then
             if card.ability.extra.xchips - card.ability.extra.xchip_loss <= 1 then
+                --Destroy joker if xchips drops too low
                 card.ability.extra.eaten = true
                 G.E_MANAGER:add_event(Event({
                     func = function()
@@ -267,6 +290,7 @@ SMODS.Joker{
                 }
 
             else
+                --remove xchips per played card
                 card.ability.extra.xchips = card.ability.extra.xchips - card.ability.extra.xchip_loss
 
                 return {
@@ -285,6 +309,10 @@ SMODS.Joker{
     end
 }
 
+--[[
+Zirconium Pants (Common)
+Creates the Trance spectral when scoring a pair of zirconium cards
+]]
 SMODS.Joker {
     key = "zirconiumpants",
     config = { extra = { odds = 3, poker_hand = "Pair" } },
@@ -311,9 +339,12 @@ SMODS.Joker {
     calculate = function(self, card, context)
         if card.debuff then return nil end
 
+        --Check conditions for enacting effect
         if context.before and context.scoring_name == card.ability.extra.poker_hand and 
             pseudorandom('likehowidance') < G.GAME.probabilities.normal/card.ability.extra.odds then
             local all_zirc = true
+
+            --Check editions of cards in scoring hand
             for _, pcard in ipairs(context.scoring_hand) do
                 if pcard.edition then
                     if not pcard.edition.tb_zirconium then
@@ -326,6 +357,7 @@ SMODS.Joker {
                 end
             end
 
+            --Generate the card if the scored hand was solely zirconium
             if all_zirc and #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
                 G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
                 G.E_MANAGER:add_event(Event({
@@ -351,6 +383,10 @@ SMODS.Joker {
     end
 }
 
+--[[
+Natural Ketchup (Common)
+Gives 20 mult. Loses 0.5 per discard.
+]]
 SMODS.Joker {
     key = "naturalketchup",
     config = { extra = { mult = 20, mult_loss = 0.5, eaten = false } },
@@ -377,6 +413,7 @@ SMODS.Joker {
 
         if not context.blueprint and context.discard then
             if card.ability.extra.mult - card.ability.extra.mult_loss <= 0 then
+                --Destroy card if mult drops too low
                 card.ability.extra.eaten = true
 
                 G.E_MANAGER:add_event(Event({
@@ -398,11 +435,13 @@ SMODS.Joker {
                         return true
                     end
                 }))
+
                 return {
                     message = localize('k_eaten_ex'),
                     colour = G.C.FILTER
                 }
             else
+                --Decrease mult otherwise
                 card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.mult_loss
                 return {
                     message = localize { type = 'variable', key = 'a_mult_minus', vars = { card.ability.extra.mult_loss } },
@@ -419,6 +458,11 @@ SMODS.Joker {
     end
 }
 
+--[[
+Variations on a Cloud (Common)
+Increases xchips payout whenever a hand of 2 stone cards is played
+Starts at 1x and increases by 0.5x
+]]
 SMODS.Joker {
     key = "cloudvariations",
     config = { extra = { xchips = 1, xchips_increase = 0.5 } },
@@ -445,6 +489,7 @@ SMODS.Joker {
         if card.debuff then return nil end
 
         if not context.blueprint and context.before then
+            --Check for stones
             local num_stone = 0
             for _, played_card in ipairs(G.play.cards) do
                 if not played_card.debuff and SMODS.has_enhancement(played_card, 'm_stone') then
@@ -452,6 +497,7 @@ SMODS.Joker {
                 end
             end
 
+            --If we have 2 stones, upgrade card.
             if num_stone == 2 then
                 card.ability.extra.xchips = card.ability.extra.xchips + card.ability.extra.xchips_increase
 
@@ -471,6 +517,10 @@ SMODS.Joker {
     end
 }
 
+--[[
+Not a Trampoline (Common)
+Gives +12 mult if played hand doesn't have gaps in rank
+]]
 SMODS.Joker {
     key = "notatrampoline",
     config = { extra = { mult = 12, no_gap = true } },
@@ -502,20 +552,25 @@ SMODS.Joker {
                 end
             end
 
+            --Copy poker hand to a dummy table as to not interfere with scoring anims
             local card_sorted = {}
             copy_table(G.play.cards, card_sorted)
 
+            --Sort by rank function
             local rank_sort = function(i, j)
                 return i:get_id() > j:get_id()
             end
-
             table.sort(card_sorted, rank_sort)
 
+            --Check for gaps
             card.ability.extra.no_gap = true
             for idx, _ in ipairs(card_sorted) do
                 if card_sorted[idx + 1] and card_sorted[idx]:get_id() - card_sorted[idx + 1]:get_id() > 1 then
-                    card.ability.extra.no_gap = false
-                    break           -- We don't need to keep looking
+                    --Check for ace and two special case
+                    if not (card_sorted[idx + 1].get_id() == 14 and card_sorted[1].get_id() == 2) then 
+                        card.ability.extra.no_gap = false
+                        break           -- We don't need to keep looking
+                    end
                 end
             end
         end
@@ -528,6 +583,11 @@ SMODS.Joker {
     end
 }
 
+--[[
+Good and evil (Common)
+Gives 84 chips and 82 mult.
+Slowly loses chips and mult overtime.
+]]
 SMODS.Joker {
     key = "goodandevil",
     config = { extra = { chips = 84, chip_loss = 2, mult = 82, mult_loss = 1, hiatus = false } },
@@ -555,6 +615,7 @@ SMODS.Joker {
         local check_death = function()
             if card.ability.extra.mult - card.ability.extra.mult_loss <= 0 and
             card.ability.extra.chips - card.ability.extra.chip_loss <= 0 then
+                --Destroy card if mult AND chips drop too low
                 G.E_MANAGER:add_event(Event({
                     func = function()
                         play_sound('tarot1')
@@ -590,6 +651,7 @@ SMODS.Joker {
                     colour = G.C.FILTER
                 }
             else
+                --Chip away at mult
                 if card.ability.extra.mult > 0 then
                     card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.mult_loss
                     return {
@@ -610,6 +672,7 @@ SMODS.Joker {
                     colour = G.C.FILTER
                 }
             else
+                --Molt away the chips
                 if card.ability.extra.chips > 0 then
                     card.ability.extra.chips = card.ability.extra.chips - card.ability.extra.chip_loss
                     return {
@@ -630,6 +693,10 @@ SMODS.Joker {
     end
 }
 
+--[[
+Miss Melody (Common)
+Gives +9 chips per played non-face card.
+]]
 SMODS.Joker {
     key = "missmelody",
     config = { extra = { chips = 9 } },
